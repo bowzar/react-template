bbe82062e77020d9823619b936f26835bd104b0e
HIDE <bowzar@qq.com>
2018-03-31 14:19:51 +0800

    update

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,19 +1,12 @@
 import React, { Component, } from 'react';
 // import ReactDom from 'react-dom';
 import "./index.less";
-import bk from "./SignInBackground.png";
-
-const styles = {
-    bgd: {
-        background: `url(${bk}) no-repeat`,
-    }
-}
 
 export default class Home extends Component {
     render() {
         return (
-            <div className="home container" style={styles.bgd}>
-                <h1 className="home header"> Hello, world, from HIDE !!!!!!!!!!! </h1>
+            <div className={"home"}>
+                <h1 className={"header"}> Hello, world, from HIDE !!!!!!!!!!! </h1>
             </div>);
     }
 }
